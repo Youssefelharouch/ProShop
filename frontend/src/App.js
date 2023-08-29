30a45ca1059c2bbf37b77960d9862bc05ccb28ab
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-08-29 01:57:37 +0100

    add some styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Container } from "react-bootstrap";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div>Store</div>
-  )
-}
+    <>
+      <Header />
+      <main className="py-3"></main>
+      <Container>
+        <h1>Welcom To ProShop</h1>
+      </Container>
+      <Footer />
 
-export default App
+    </>
+  );
+};
+
+export default App;
