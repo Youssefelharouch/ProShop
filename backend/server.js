2ebe5004f4116849e987257d9dbd7835114d1df3
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-08-29 20:10:19 +0100

    add models and connection

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from "express";
 import products from './data/products.js';
 import dotenv from 'dotenv'
 dotenv.config();
+import connectDB from "./config/db.js";
 
 
+connectDB();
 const port = process.env.PORT || 5000; 
 
 const app = express();
