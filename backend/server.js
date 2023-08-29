17fe9404ca1faaed26cdfc93194ca174997fd755
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-08-29 14:15:59 +0100

    fetch data from the back end

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import products from './data/products.js';
+import dotenv from 'dotenv'
+dotenv.config();
+
+
+const port = process.env.PORT || 5000; 
+
+const app = express();
+
+app.get("/api/products", (req, res) => {
+  res.send(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find(p=>p._id === req.params.id );
+  res.send(product);
+});
+
+app.listen(port, (params) => {
+  console.log("server runing on port 5000");
+});
