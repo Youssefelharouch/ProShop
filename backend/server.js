a4d00e49dac15e4921d4d23ac0c8c3b678ba2b89
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-08-30 20:41:44 +0100

    User auth endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 import express from 'express';
 
 import productRoutes from './routes/productRoute.js';
+import userRoutes from './routes/userRoutes.js';
 import connectDB from "./config/db.js";
 import { notFound,errorHandler } from './middleware/ErrorModdleware.js'
 
@@ -9,10 +10,16 @@ dotenv.config();
 
 
 const app = express();
+
+// Body pARSER middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 connectDB();
 const port = process.env.PORT || 5000; 
 
 app.use('/api/products',productRoutes);
+app.use('/api/users',userRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
