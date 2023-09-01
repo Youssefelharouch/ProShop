426ac4b7422484935cb69bd7eb493605f5c70352
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-09-02 00:43:40 +0100

    orders  Api's & Orders screen

--- frontend/src/slices/carteSlice.js
+++ frontend/src/slices/carteSlice.js
@@ -4,8 +4,7 @@ import { updateCart } from "../utils/cartUtils";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
-  : { cartItems: [], shippingAddress: {}, paymentMethod: 'PayPal' };
-
+  : { cartItems: [], shippingAddress: {}, paymentMethod: "PayPal" };
 
 const cartSlice = createSlice({
   name: "cart",
@@ -38,7 +37,19 @@ const cartSlice = createSlice({
       state.paymentMethod = action.payload;
       return updateCart(state);
     },
+    clearCartItems: (state, action) => {
+      state.cartItems = [];
+      return updateCart(state);
+    },
   },
 });
-export const { addToCart, removeFromCart , saveShippingAddress,savePaymentMethod } = cartSlice.actions;
+export const {
+  addToCart,
+  removeFromCart,
+  saveShippingAddress,
+  savePaymentMethod,
+  clearCartItems,
+} = cartSlice.actions;
 export default cartSlice.reducer;
+
+
