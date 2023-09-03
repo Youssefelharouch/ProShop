accbb729b2f4407b43db0bdcd339048899933354
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-09-03 10:05:02 +0100

    Edit Products functionality

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,12 +1,13 @@
 import  express  from "express";
 const router = express.Router();
-import {getProducts,getSingleProduct,createProduct} from '../controllers/productController.js';
+import {getProducts,getSingleProduct,createProduct,updateProduct} from '../controllers/productController.js';
 import { protect,admin } from '../middleware/authModdleware.js';
 
 
 router.route('/').get(getProducts);
-router.route('/:id').get(getSingleProduct);
+router.route('/:id').get(getSingleProduct).put(protect, admin, updateProduct);
 router.route('/').get(getProducts).post(protect, admin, createProduct);
+
 
 
 
