1bdaef33e1c08352b0105ee3c3fd055cd2d1e021
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-09-03 09:44:53 +0100

    create new  empty Product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -19,9 +19,30 @@ const getSingleProduct = asyncHandler(async (req, res) => {
   if(product)res.json(product);
   else {
     res.status(404);
-    throw new Eroor ('Product not found');
+    throw new Error('Product not found');
   }
 
 });
 
-export  {getProducts,getSingleProduct}
+// @desc    Create a product
+// @route   POST /api/products
+// @access  Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Products({
+    name: 'Sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+
+export  {getProducts,getSingleProduct,createProduct}
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,13 @@
 import  express  from "express";
 const router = express.Router();
-import {getProducts,getSingleProduct} from '../controllers/productController.js';
+import {getProducts,getSingleProduct,createProduct} from '../controllers/productController.js';
+import { protect,admin } from '../middleware/authModdleware.js';
+
 
 router.route('/').get(getProducts);
 router.route('/:id').get(getSingleProduct);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+
 
 
 export default router
