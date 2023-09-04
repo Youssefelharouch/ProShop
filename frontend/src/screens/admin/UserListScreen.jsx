351ad3842a85dea7558cba5df6559b537312d563
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-09-04 11:24:35 +0100

    Admin Delete User

--- frontend/src/screens/admin/UserListScreen.jsx
+++ frontend/src/screens/admin/UserListScreen.jsx
@@ -2,15 +2,28 @@ import React from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Table, Button } from 'react-bootstrap';
 import { FaTrash, FaEdit, FaCheck, FaTimes } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+
+
 import Message from '../../components/Message';
 import Loader from '../../components/Loader';
-import { useGetUsersQuery } from '../../slices/usersApiSlice';
+import { useDeleteUserMutation, useGetUsersQuery } from '../../slices/usersApiSlice';
 
 const UserListScreen = () => {
   const { data: users, refetch, isLoading, error } = useGetUsersQuery();
+  const [deleteUser] = useDeleteUserMutation();
+
 
   const deleteHandler = async (id) => {
-    console.log('delete');
+    if (window.confirm('Are you sure')) {
+      try {
+        await deleteUser(id);
+        toast.success('User deleted');        
+        refetch();
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+      }
+    }
   };
 
   return (
