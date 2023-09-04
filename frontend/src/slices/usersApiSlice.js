c703e5835f905463d68fd0b3a28ca2d714921422
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-09-04 11:15:19 +0100

    List users for admin

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,38 +1,49 @@
-import { USERS_URL } from '../constants';
-import { apiSlice } from './apiSlice';
+import { USERS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
 
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
-      query: ( data ) => ({
+      query: (data) => ({
         url: `${USERS_URL}/auth`,
-        method:'POST',
-        body:data
-      })
+        method: "POST",
+        body: data,
+      }),
     }),
     register: builder.mutation({
-      query: ( data ) => ({
+      query: (data) => ({
         url: `${USERS_URL}`,
-        method:'POST',
-        body:data
-      })
+        method: "POST",
+        body: data,
+      }),
     }),
     logout: builder.mutation({
       query: () => ({
         url: `${USERS_URL}/logout`,
-        method:'POST',
-      })
+        method: "POST",
+      }),
     }),
     profile: builder.mutation({
       query: (data) => ({
         url: `${USERS_URL}/profile`,
-        method:'PUT',
-        body:data,
-      })
-    })
-
-  
+        method: "PUT",
+        body: data,
+      }),
+    }),
+    getUsers: builder.query({
+      query: () => ({
+        url: `${USERS_URL}`,
+      }),
+      providesTags: ["User"],
+      keepUnusedDataFor: 5,
+    }),
   }),
 });
 
-export const { useLoginMutation,useLogoutMutation,useRegisterMutation,useProfileMutation } = usersApiSlice;
+export const {
+  useLoginMutation,
+  useLogoutMutation,
+  useRegisterMutation,
+  useProfileMutation,
+  useGetUsersQuery,
+} = usersApiSlice;
