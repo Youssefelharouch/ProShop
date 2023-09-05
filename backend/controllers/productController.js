1f241daa4f955466c08e037676591accfb77a8b7
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-09-05 18:48:42 +0100

    production script

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,7 +6,7 @@ import Products from "../models/productModel.js";
 // @access Public
 
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 12;
+  const pageSize = 8;
   const page = Number(req.query.pageNumber) || 1;
   const keyword = req.query.keyword
   ? {
